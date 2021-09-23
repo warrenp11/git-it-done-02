e51841e7a359f1d9872bbf6da37f54905537c434
Peter <warrenp11@gmail.com>
2021-09-23 11:51:36 -0400

    add form to search for a github user's repositories

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,20 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+}
+
 var getUserRepos = function(user) {
 
     // format the github api url
@@ -11,4 +28,4 @@ var getUserRepos = function(user) {
     });
 };
 
-getUserRepos("warrenp11");
+userFormEl.addEventListener("submit", formSubmitHandler);

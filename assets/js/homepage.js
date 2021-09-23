fcef9b15fdbe4a67ba79f3dff63b5817d9f07f3a
Peter <warrenp11@gmail.com>
2021-09-23 15:05:45 -0400

    user search returns list of repos with number of issues, add icon for issues

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,8 @@
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -32,6 +35,63 @@ var getUserRepos = function(user) {
 var displayRepos = function(repos, searchTerm) {
     console.log(repos);
     console.log(searchTerm);
+
+    // clear old content
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+
+    // loop over repos
+    for (var i = 0; i < repos.length; i++) {
+        // format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+        console.log(repoName);
+
+        // create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flew-row justify-space-between align-center";
+
+        // create a span element to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        // append to containier
+        repoEl.appendChild(titleEl);
+
+        //  create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        // check if current repo has any issues
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML = 
+                "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+
+        // append open issues to container
+        repoEl.appendChild(statusEl);
+
+        // append container to the dom
+        repoContainerEl.appendChild(repoEl);
+    }
 };
+
+
+
+// *************************************************
+
+/*Let's identify how the details we need are labeled in the response data. We're looking for the name property (for the repository name), the open_issues_count property (for the number of issues), and the login property in the nested owner object (for the repository owner).*/
+
+// *************************************************
+
+
+
+
+
+
+
+
+
 
 userFormEl.addEventListener("submit", formSubmitHandler);

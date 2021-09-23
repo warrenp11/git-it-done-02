e26cafe0a4640a4dbeab20a44275cfd25535a7d3
Peter <warrenp11@gmail.com>
2021-09-23 11:32:39 -0400

    update getUserRepos request logic with dynamic variables

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,12 +1,14 @@
-var getUserRepos = function() {
-    console.log("function was called"); // to make sure function worked
+var getUserRepos = function(user) {
 
-    // fetch
-    var response = fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
             console.log(data);
         });
     });
 };
 
-getUserRepos();
+getUserRepos("warrenp11");
